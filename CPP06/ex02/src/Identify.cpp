42a55befda17b269296039e12e9f2245591edbef
Kilian Tintim Goncalves <ktintim-@c2r5s3.42lausanne.ch>
2025-09-30 10:57:07 +0200

    fix

--- CPP06/ex02/src/Identify.cpp
+++ CPP06/ex02/src/Identify.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Identify.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kilian <kilian@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ktintim <ktintim-@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 16:48:18 by kilian            #+#    #+#             */
-/*   Updated: 2025/08/03 17:52:42 by kilian           ###   ########.fr       */
+/*   Updated: 2025/09/30 10:56:30 by ktintim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,36 @@ void identify(Base* p)
 	if (p == dynamic_cast<A*>(p))
 		std::cout << "It's an A object" << std::endl;
 	if (p == dynamic_cast<B*>(p))
-		std::cout << "It's an B object" << std::endl;
+		std::cout << "It's a B object" << std::endl;
 	if (p == dynamic_cast<C*>(p))
-		std::cout << "It's an C object" << std::endl;
+		std::cout << "It's a C object" << std::endl;
 }
 
 void identify(Base& p)
 {
-	if (&p == dynamic_cast<A*>(&p))
+	try
+	{
+		(void)dynamic_cast<A&>(p);
 		std::cout << "It's an A object" << std::endl;
-	if (&p == dynamic_cast<B*>(&p))
-		std::cout << "It's an B object" << std::endl;
-	if (&p == dynamic_cast<C*>(&p))
-		std::cout << "It's an C object" << std::endl;
+	}
+	catch(...)
+	{
+		try
+		{
+			(void)dynamic_cast<B&>(p);
+			std::cout << "It's a B object" << std::endl;
+		}
+		catch(...)
+		{
+			try
+			{
+				(void)dynamic_cast<C&>(p);
+				std::cout << "It's a C object" << std::endl;
+			}
+			catch(...)
+			{
+				std::cout << "None of ABC" << std::endl;
+			}
+		}
+	}
 }
