6b58aa0cf30a4a8e00a3c3bb5f19e99fc40e91aa
Kilian Tintim Goncalves <ktintim-@c2r4s1.42lausanne.ch>
2025-10-06 14:25:17 +0200

    fix if span size < 2

--- CPP08/ex01/src/Span.cpp
+++ CPP08/ex01/src/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: ktintim <ktintim-@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 15:32:43 by ktintim           #+#    #+#             */
-/*   Updated: 2025/10/03 12:02:04 by ktintim          ###   ########.fr       */
+/*   Updated: 2025/10/06 14:23:47 by ktintim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,9 @@ void Span::addRangeNumber(int first, int last)
 
 int Span::shortestSpan()
 {
+	if (_size < 2)
+		return 0;
+	
 	int ret = __INT_MAX__;
 	std::vector<int> cpy;
 
@@ -100,6 +103,9 @@ int Span::shortestSpan()
 
 int Span::longestSpan()
 {
+	if (_size < 2)
+		return 0;
+
 	std::vector<int> cpy;
 
 	cpy = _array;
